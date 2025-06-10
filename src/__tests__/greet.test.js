4fe5ab8ee03b2d04b9c0f02f551f7b003d63d0dc
Sebastien Poyer <sebastien.poyer@planity.com>
2025-06-10 16:10:58 +0200

    fixup! test(greet): add unit test for greet

--- src/__tests__/greet.test.js
+++ src/__tests__/greet.test.js
@@ -3,3 +3,7 @@ const { greet } = require('../greet');
 test('greet returns correct greeting', () => {
   expect(greet('Alice')).toBe('Hello, Alice!');
 });
+
+test('greet with default name', () => {
+  expect(greet()).toBe('Hello, stranger!');
+});
