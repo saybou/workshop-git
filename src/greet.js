6bf053a39214ec18120ac2a5e5d699527f41eeed
Sebastien Poyer <sebastien.poyer@planity.com>
2025-06-11 11:30:01 +0200

    feat(greet): implement greeting feature

--- src/greet.js
+++ src/greet.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const greet = (name) => {
-  // TODO: implement the greet function
+const greet = (name = 'stranger') => {
+  return `Hello, ${name}!`;
 }
 
 module.exports = { greet };
